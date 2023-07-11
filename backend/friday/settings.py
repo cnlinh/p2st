ab1ec138c4b7b4bfac0a72aa702b2c30c86c8f91
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2023-07-10 22:42:10 -0700

    add docker-compose, dockerfile, and postgres

--- backend/friday/settings.py
+++ backend/friday/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "friday.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": "5432",
     }
 }
 
