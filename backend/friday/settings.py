7dac59527ceccbcdee4ba905309c488d6a70a0e1
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2023-07-12 00:45:23 -0700

    create and use custom User model

--- backend/friday/settings.py
+++ backend/friday/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "authuser",
 ]
+
+AUTH_USER_MODEL = "authuser.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
