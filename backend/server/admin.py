80d9f597682d09ac8a8432de3a3967f190138a34
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2023-07-16 02:49:16 -0700

    register models to admin site

--- backend/server/admin.py
+++ backend/server/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Topic, Conversation, Question, UserQuestion, Answer, Recommendation
 
-# Register your models here.
+admin.site.register(Topic)
+admin.site.register(Conversation)
+admin.site.register(Question)
+admin.site.register(UserQuestion)
+admin.site.register(Answer)
+admin.site.register(Recommendation)
