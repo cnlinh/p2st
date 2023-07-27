59a9021c9b6d720e782af91ec316f9651ace67bc
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2023-07-27 12:31:01 +0700

    add logging configs

--- backend/friday/settings.py
+++ backend/friday/settings.py
@@ -141,3 +141,17 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication"
     ]
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",  # change logging level here
+    },
+}
