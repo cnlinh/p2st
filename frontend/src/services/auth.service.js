3a1498902bddd80472b394833fa88a0d117f59a4
cnlinh <linh.caongoc12@gmail.com>
2023-08-14 10:41:12 +0800

    Implement refresh token

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -2,10 +2,42 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8000/api/';
 
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    const originalRequest = error.config;
+    
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      
+      const refreshToken = JSON.parse(sessionStorage.getItem('refresh'));
+      
+      if (!refreshToken) throw error;
+      
+      return axios.post(`${API_URL}token/refresh`, { refresh: refreshToken })
+        .then(res => {
+          sessionStorage.setItem('token', JSON.stringify(res.data.access));
+          
+          axios.defaults.headers['Authorization'] = 'Bearer ' + res.data.access;
+          originalRequest.headers['Authorization'] = 'Bearer ' + res.data.access;
+          
+          return axios(originalRequest);
+        })
+        .catch(err => {
+          console.error("Error fetching refresh token:", err);
+          AuthService.logout();
+          window.location.href = '/login';
+        });
+    }
+    
+    return Promise.reject(error);
+  }
+);
+
 class AuthService {
   login(user) {
     return axios
-      .post(API_URL + 'login', {
+      .post(`${API_URL}login`, {
         email: user.username,
         password: user.password
       })
@@ -20,6 +52,24 @@ class AuthService {
       });
   }
 
+  refreshToken() {
+    const refreshToken = JSON.parse(sessionStorage.getItem('refresh'));
+
+    return axios
+      .post(`${API_URL}token/refresh`, { refresh: refreshToken })
+      .then(response => {
+        if (response.data) {
+          sessionStorage.setItem('token', JSON.stringify(response.data.access));
+        }
+
+        return response.data;
+      })
+      .catch(error => {
+        console.error("Error during token refresh:", error);
+        throw error;
+      });
+  }
+
   logout() {
     sessionStorage.removeItem('refresh');
     sessionStorage.removeItem('token');
@@ -27,7 +77,7 @@ class AuthService {
   }
 
   register(user) {
-    return axios.post(API_URL + 'register', {
+    return axios.post(`${API_URL}register`, {
       name: user.name,
       email: user.email,
       student_id: user.studentId,
