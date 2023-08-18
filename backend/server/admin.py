2e56c5bca4528a10a20209cf9fe7289fc9b2f2fa
cnlinh <linh.caongoc12@gmail.com>
2023-08-18 15:54:16 +0800

    Update topic list

--- backend/server/admin.py
+++ backend/server/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import Topic, Conversation, Question, Message, Answer, Rating
+from .models import (
+    Module, Enrollment, Topic, Conversation, 
+    Question, Answer, Message, Rating
+)
 
+admin.site.register(Module)
+admin.site.register(Enrollment)
 admin.site.register(Topic)
 admin.site.register(Conversation)
 admin.site.register(Question)
-admin.site.register(Message)
 admin.site.register(Answer)
+admin.site.register(Message)
 admin.site.register(Rating)
