1e5529c99d739c59e0c79b437d28d02f7fdbdfe0
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2023-08-26 17:49:17 +0800

    add multi column index on enrollments

--- backend/server/migrations/0002_remove_enrollments_fk.up.sql
+++ backend/server/migrations/0002_remove_enrollments_fk.up.sql
@@ -5,5 +5,8 @@ DROP COLUMN user_id;
 ALTER TABLE enrollments
 ADD COLUMN student_id VARCHAR(9) NOT NULL;
 
+ALTER TABLE enrollments
+ADD UNIQUE (student_id, module_id);
+
 ALTER TABLE modules
 ADD UNIQUE (code);
